0f11c302e9ba7212eb1f2d81c0313be81dcc15c0
ElenaKorenieva <olena.korenieva@mail.com>
2022-05-22 22:39:36 +0200

    feat: delete files

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let menuBtn = document.querySelector('.button');
-    let menu = document.querySelector('.start_header');
-    menuBtn.addEventListener('click', function() {
-        menu.classList.toggle('visible');
-    })
-})
